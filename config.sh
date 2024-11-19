edbc90e00e46deb85ca609669ce0f539fc3cbb83
Stefán Jóhannsson <sevensteves@proton.me>
2024-11-19 23:41:08 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,5 @@
 # runner_config.sh
-
-GITHUB_ORGANIZATION="your_organization"
+GITHUB_ORGANIZATION="sevensteves-com"
 GITHUB_PAT="your_github_pat"
 RUNNER_VERSION="2.320.0"
-RUNNER_DIR="/srv/actions-runner"
+RUNNER_DIR="~/actions-runner"
